4a52dbe48c9e423126879fe87d57ef72a81ae2cb
Maria Topalova <odegova.masha95@gmail.com>
2022-04-13 15:42:06 +0300

    dont work image container

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,141 @@
-const profilePopup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close');
-const openProfileButton = document.querySelector('.profile__edit-button');
-const formPopup = document.querySelector('.popup__form');
+const profilePopupOpened = 'popup_is-active';
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
 const profileName = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const popupName = formPopup.querySelector('.popup__input_type_name');
-const popupJob = formPopup.querySelector('.popup__input_type_job');
-const profilePopupOpened = 'popup_is-active';
 
-function openPopup () {
-    popupName.value = profileName.textContent;
-    popupJob.value = profileSubtitle.textContent;
-    profilePopup.classList.add(profilePopupOpened);
+const elements = document.querySelector('.elements');
+
+const popupEditButton = document.querySelector('.popup_type_edit-button');
+const popupAddButton = document.querySelector('.popup_type_add-button');
+const popupOpenImage = document.querySelector('.popup_type_image');
+const popupContainerImage = popupOpenImage.querySelector('.popup__container_image');
+const popupForm = popupContainerImage.querySelector('.popup__form');
+const popupImage = popupForm.querySelector('.popup__image');
+const popupText = popupForm.querySelector('.popup__text');
+const popupCloseEditButton = document.querySelector('.popup__close_type_edit-button');
+const popupCloseAddButton = document.querySelector('.popup__close_type_add-button');
+const popupCloseImage = document.querySelector('.popup__close_type_image');
+const popupFormEditButton = popupEditButton.querySelector('.popup__form_type_edit-button');
+const popupFormAddButton = popupAddButton.querySelector('.popup__form_type_add-button');
+const popupCardName = popupFormAddButton.querySelector('.popup__input_type_card-name');
+const popupLink = popupFormAddButton.querySelector('.popup__input_type_link');
+const popupName = popupFormEditButton.querySelector('.popup__input_type_name');
+const popupJob = popupFormEditButton.querySelector('.popup__input_type_job');
+
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+//открытие
+function openPopup(pop) {
+  pop.classList.add(profilePopupOpened)
 }
-openProfileButton.addEventListener('click', openPopup);
 
+profileEditButton.addEventListener('click', function() {
+  popupName.value = profileName.textContent;
+  popupJob.value = profileSubtitle.textContent;
+  openPopup(popupEditButton)
+})
 
-function closePopup () {
-    profilePopup.classList.remove(profilePopupOpened);
+profileAddButton.addEventListener('click', function() {
+  openPopup(popupAddButton)
+})
+
+//закрытие
+function closePopup(pop) {
+  pop.classList.remove(profilePopupOpened)
 }
-closePopupButton.addEventListener('click', closePopup);
 
+popupCloseEditButton.addEventListener('click', function() {
+  closePopup(popupEditButton)
+})
 
-function submitForm (e) {
-    e.preventDefault();
-    profileName.textContent = popupName.value;
-    profileSubtitle.textContent = popupJob.value;
-    closePopup();
+popupCloseAddButton.addEventListener('click', function() {
+  closePopup(popupAddButton)
+})
+
+popupCloseImage.addEventListener('click', function() {
+  closePopup(popupOpenImage)
+})
+
+function submitForm(e) {
+  e.preventDefault();
+  profileName.textContent = popupName.value;
+  profileSubtitle.textContent = popupJob.value;
+  closePopup(popupEditButton)
 }
-formPopup.addEventListener('submit', submitForm);
+
+popupFormEditButton.addEventListener('submit', submitForm);
+
+function createCard(inputName, inputLink) {
+  const elementTemplate = document.querySelector('#element').content;
+  const elementInfo = elementTemplate.querySelector('.element__info').cloneNode(true);
+  const elementImage = elementInfo.querySelector('.element__image');
+
+  elementInfo.querySelector('.element__title').textContent = inputName;
+  elementImage.src = inputLink;
+  elementImage.alt = inputName;
+
+  elementInfo.querySelector('.element__button-trash').addEventListener('click', function() {
+    elementInfo.remove();
+  })
+
+  elementInfo.querySelector('.element__button').addEventListener('click', function(event) {
+    event.target.classList.toggle('element__button_active');
+  })
+
+  elementImage.addEventListener('click', function() {
+    openPopup(popupOpenImage);
+    popupImage.src = inputLink;
+    popupImage.alt = inputName;
+    popupText.textContent = inputName;
+  })
+
+  return elementInfo
+}
+
+function createItem(inputName, inputLink) {
+  const cardItem = createCard(inputName, inputLink)
+  elements.prepend(cardItem);
+}
+
+function addItem(evt) {
+  evt.preventDefault();
+  const inputName = popupCardName.value;
+  const inputLink = popupLink.value;
+  createItem(inputName, inputLink);
+  closePopup(popupAddButton)
+  popupFormAddButton.reset()
+}
+
+initialCards.forEach(function(item) {
+  createItem(item.name, item.link);
+})
+
+popupFormAddButton.addEventListener('submit', addItem);
